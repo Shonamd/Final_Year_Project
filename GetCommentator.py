a397d548e1969bad4e064cee711bbdcf85e31de3
Shonamd <shonamdoran@gmail.com>
2019-01-31 16:08:46 +0000

    Finds the words spoken in the video and how many

---
+++ GetCommentator.py
@@ -0,0 +1,61 @@
+import speech_recognition as sr
+import os
+
+dire = 'audio'
+
+def ConvertAudio(audio, x):
+	r = sr.Recognizer()
+
+	with sr.AudioFile(audio) as source :
+		audio = r.record(source)
+
+	try:
+		text = r.recognize_google(audio)
+
+		file = open("text/speeches_" + str(x) + ".txt", "w+")
+
+		file.write(text)
+
+		file.close()
+
+		print("worked")
+
+		data = open("text/speeches_" + str(x) + ".txt", 'r').read()
+
+		wordcount = len(data.split())
+
+		print(wordcount)
+
+		#return(text)
+
+	except Exception as e:
+		print(e)
+
+# for x in range(3):
+# 	ConvertAudio('audio/audio_output_' + str(x + 10) + '.wav')
+
+
+
+#ConvertAudio('audio/audio_output_20.wav', 0)
+
+def StartCon():
+	x = 0
+	
+	for filenames in os.listdir('audio'):
+		if filenames.endswith(".wav"):
+			#print(filenames)
+			ConvertAudio('audio/'+filenames, x)
+			x += 1
+
+StartCon()
+
+# for filenames in os.listdir(dire):
+# 	print(filenames)
+
+# 	#add_clips()
+
+# 	ConvertAudio(dire + '/' + filenames, x)
+
+
+
+	#print("\n")
